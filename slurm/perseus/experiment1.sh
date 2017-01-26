3c0f475227e5815e61287046998dd478032f220e
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-01-26 11:38:46 -0500

    change args

--- slurm/perseus/experiment1.sh
+++ slurm/perseus/experiment1.sh
@@ -8,23 +8,22 @@
 #SBATCH --mail-type=begin       # email me when the job starts
 #SBATCH --mail-type=end         # email me when the job finishes
 
-cd /tigress/adrianp/projects/thejoker/scripts/
+cd /tigress/adrianp/projects/thejoker-paper/scripts/
 
 module load openmpi/gcc/1.10.2/64
 
-source activate thejoker
+source activate thejoker-paper
 
 # Run experiment 1!
 # python make-experiment1-data.py -s 1234
 
+srun python run-sampler.py -v --mpi -o \
+-n 2**28 -s 42 \
+-f ../cache/experiment1.h5
+
+# TODO: I need a way to save to a different key in the output file
 # srun python run-sampler.py -v --mpi -o \
 # -n 2**28 -s 42 \
 # -f ../data/experiment1.h5 \
-# --name="experiment1-fixed-jitter.h5" \
-# --fixed-jitter='0 m/s'
-
-srun python run-sampler.py -v --mpi -o \
--n 2**28 -s 42 \
--f ../data/experiment1.h5 \
---name="experiment1-sample-jitter.h5" \
---log-jitter2-mean=0 --log-jitter2-std=8 --jitter-unit='m/s'
+# --name="experiment1-sample-jitter.h5" \
+# --log-jitter2-mean=0 --log-jitter2-std=8 --jitter-unit='m/s'
